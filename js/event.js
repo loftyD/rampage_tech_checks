e9d77843efb91de2632cf1266a74324b6aa0119f
Ben Hassan <loftyd@live.co.uk>
2022-10-27 23:02:31 +0100

    previous history include

--- js/event.js
+++ js/event.js
@@ -1,7 +1,6 @@
 if(!isAuthed) {
 	window.location.replace("/login");
 } else {
-
 	var selected_event = '';
 	var selected_robot = '';
 	var robot_data = [];
@@ -134,75 +133,149 @@ if(!isAuthed) {
 		authPost("/techchecks/create", JSON.stringify(data), function(data) {
 			if(data.hasOwnProperty('status')) {
 				alert(data.status);
+				$("#bot-info").html('');
 				$('a[href="#robot"]').tab('show');
+				$("#robot_select > option").remove();
+				$("#robot_select").append("<option value=''></option>");
+			    authGet("/events/permissible_robots?event_id="+selected_event, function(data) {
+			    	for(i=0;i<data.length;i++) {
+			    		robot_data[data[i].id] = data[i];
+			    		$("#robot_select").append("<option data-reserve='"+ data[i].reserve + "' value='"+ data[i].id+"'>"+ data[i].name +"</option>");
+			    	}
+
+			    	$("#robot_select").select2({
+						theme: "bootstrap",
+						placeholder: "Please Select",
+						width: "100%"
+					});
+			    });
 			}
 		});
 	})
-
 
 	$("#robot_select").on('change', function() {
 		let techcheck = false;
 		// $("#bot-info").text("Is Reserve: " + $(this).find(":selected").data('reserve'));
 		$("#bot-info").html('');
 		selected_robot = $(this).val();
-		data = robot_data[selected_robot][0];
-
-		$("#bot-info").append("<ul>");
-		for(key in data) {
-			okey = key;
-			dt = data[okey];
-			if(okey == 'id') {
-				continue;
-			}
-			if(okey == 'inspector' && dt == false) {
-				continue;
-			}
-
-			if(okey == 'techcheck') {
-				key="Has Been Tech Checked"
-			}
-
-			if(okey == 'techcheck') {
-				techcheck = dt;
-				if(typeof data[okey] === 'object') {
-					dt = 'Yes';
-				} else {
-					dt = 'No';
-				}
-			}
-
-			$("#bot-info").append("<li>"+ key.toProperCase() +": "+ dt +"</li>");
-		}
-		$("#bot-info").append("</ul>");
-
-		if(typeof techcheck === 'object') {
-			$("#bot-info").append("<hr><h2>Recent Tech Check History</h2>");
-			$("#bot-info").append("<ul>");
-			for(k in techcheck) {
-				val = techcheck[k];
-				key = k;
-				if(k == 'id' || k =='series_id' || k == 'robot_id' || k == 'inspector_id') {
-					continue;
-				}
-				if (k.match(/^check_/)) {
-					if(val == '1') {
-						val = 'Yes';
-					} else {
-						val = 'No';
+		let data = robot_data[selected_robot];
+		let recentTechCheck = null;
+		authGet("/techchecks/history?robot_id="+selected_robot, function(data_hist) {
+			if(data_hist !== null) {
+				recentTechCheck = data_hist;
+				$("#bot-info").append("<div class='row'><div class='col-sm-12'><ul id='main_tc_details'>");
+				for(key in data) {
+					okey = key;
+					dt = data[okey];
+					if(okey == 'id' || key == "comments") {
+						continue;
 					}
+					if(okey == 'inspector' && dt == false) {
+						continue;
+					}
+
+					if(okey == 'techcheck') {
+						key="Has Been Tech Checked"
+					}
+
+					if(okey == 'has_passed') {
+						key = 'Has Passed Tech Checks';
+						if(dt == null) {
+							continue;
+						} else {
+							if(dt == true) {
+								dt = 'PASS';
+							} else {
+								dt = 'FAIL';
+							}
+						}
+					}
+
+					if(okey == 'techcheck') {
+						techcheck = dt;
+						if(typeof data[okey] === 'object') {
+							dt = 'Yes';
+						} else {
+							dt = 'No';
+						}
+					}
+
+				$("#main_tc_details").append("<li>"+ key.toProperCase() +": "+ dt +"</li>");
 				}
 
-				if (k == 'created_at') {
-					key = 'Tech Check Created At';
+				$("#bot-info").append("<div class='row' id='tc_panel'><div class='col-sm-6' id='main_tc'>");
+
+				getTechCheck(data);
+				if(recentTechCheck != null) {
+					$("#tc_panel").append("<div class='col-sm-6' id='hist_tc'>");
+					$("#hist_tc").append("<hr><h3 data-toggle='tooltip' data-placement='top' title='"+ recentTechCheck.techcheck.event + "'>Previous Event Tech Check History</h3><br /><br />");
+					$("#hist_tc").append("<ul id='hist_tc_list'>");
+					recentTechCheck = recentTechCheck.techcheck;
+					for(key in recentTechCheck) {
+						okey = key;
+						if(key == 'event' || key == 'comments' || key == 'inspector') {
+							continue;
+						}
+						if(okey == 'last_tech_checked_at') {
+							key = 'Last Tech Checked At'
+						}
+						dt = recentTechCheck[okey];
+						if(dt == '1') {
+							dt = 'Yes';
+						} else if(dt == '0') {
+							dt = 'No';
+						}
+						re = /_/gi;
+						$("#hist_tc_list").append("<li>"+ key.toProperCase().replace(re,' ') +": "+ dt +"</li>");
+					}
+					$("#hist_tc").append("<h4>Comments</h4><blockquote>" + data_hist.techcheck.comments +"</blockquote><br /><br /><strong>Inspected By: "+data_hist.techcheck.inspector+"</strong>");	
 				}
-				if (k == 'updated_at') {
-					key = 'Last Updated At';
+			} else {
+				$("#bot-info").append("<div class='row'><div class='col-sm-12'><ul id='main_tc_details'>");
+				for(key in data) {
+					okey = key;
+					dt = data[okey];
+					if(okey == 'id' || okey == 'comments') {
+						continue;
+					}
+					if(okey == 'inspector' && dt == false) {
+						continue;
+					}
+
+					if(okey == 'techcheck') {
+						key="Has Been Tech Checked"
+					}
+
+					if(okey == 'has_passed') {
+						key = 'Has Passed Tech Checks';
+						if(dt == null) {
+							continue;
+						} else {
+							if(dt == true) {
+								dt = 'PASS';
+							} else {
+								dt = 'FAIL';
+							}
+						}
+					}
+
+					if(okey == 'techcheck') {
+						techcheck = dt;
+						if(typeof data[okey] === 'object') {
+							dt = 'Yes';
+						} else {
+							dt = 'No';
+						}
+					}
+
+				$("#main_tc_details").append("<li>"+ key.toProperCase() +": "+ dt +"</li>");
+				}				
+				$("#bot-info").append("<div class='row' id='tc_panel'><div class='col-sm-12' id='main_tc'>");
+				if(typeof data === 'object') {
+					getTechCheck(data);
 				}
-				re = /_/gi;
-				$("#bot-info").append("<li>"+ key.toProperCase().replace(re,' ') +": "+ val +"</li>");
 			}
-			$("#bot-info").append("</ul>");
-		}
+		});	
 
 		$(".btn-checks-continue").removeClass("d-none");
 	});
@@ -219,12 +292,14 @@ if(!isAuthed) {
 	    $('.nav-tabs .active').parent().next('li').find('a').removeClass('disabled').trigger('click');
 	    authGet("/events/permissible_robots?event_id="+selected_event, function(data) {
 	    	for(i=0;i<data.length;i++) {
-	    		robot_data[data[i].id] = data;
+	    		robot_data[data[i].id] = data[i];
 	    		$("#robot_select").append("<option data-reserve='"+ data[i].reserve + "' value='"+ data[i].id+"'>"+ data[i].name +"</option>");
 	    	}
+
 	    	$("#robot_select").select2({
 				theme: "bootstrap",
-				placeholder: "Please Select"
+				placeholder: "Please Select",
+				width: "100%"
 			});
 	    });
 	});
@@ -235,4 +310,39 @@ if(!isAuthed) {
 				<td><a href='javascript:void(0)' class='btn btn-primary btn-event' data-event='"+data[i].id+"'>Select</a></td>/tr>");
 		}
 	}, null);
+}
+
+
+
+function getTechCheck(data) {
+	if(typeof data === 'object' && data.techcheck) {
+		$("#main_tc").append("<hr><h3>Recent Tech Check History For This Event</h3><br /><br />");
+		$("#main_tc").append("<ul id='main_tc_list'>");
+		for(k in data.techcheck) {
+			val = data.techcheck[k];
+			key = k;
+			if(k == 'id' || k =='series_id' || k == 'robot_id' || k == 'inspector_id' || k == 'comments') {
+				continue;
+			}
+			if (k.match(/^check_/)) {
+				if(val == '1') {
+					val = 'Yes';
+				} else {
+					val = 'No';
+				}
+			}
+
+			if (k == 'created_at') {
+				key = 'Tech Check Created At';
+			}
+			if (k == 'updated_at') {
+				key = 'Last Updated At';
+			}
+			re = /_/gi;
+			$("#main_tc_list").append("<li>"+ key.toProperCase().replace(re,' ') +": "+ val +"</li>");
+		}
+		if(data.techcheck.comments) {
+			$("#main_tc").append("<h4>Comments</h4><blockquote>" + data.techcheck.comments +"</blockquote><br /><br /><strong>Inspected By: "+data.inspector+"</strong>");
+		}
+	}
 }
--- views/robot-select.php
+++ views/robot-select.php
@@ -9,8 +9,9 @@
 						<label for="robot_select">Robot</label>
 						<select id="robot_select" class="form-control form-control-lg" required autofocus></select>
 						<br />
-						<p id="bot-info"></p>
-						<a href="javascript:void(0)" class="btn btn-primary btn-checks-continue d-none">Proceed to checks</a>
+						<a href="javascript:void(0)" class="btn btn-primary btn-checks-continue d-none">Proceed to checks...</a>
+						<hr />
+						<div id="bot-info"></div>
 					</div>
 				</div>
 			</form>
